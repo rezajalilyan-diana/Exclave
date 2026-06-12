1de50479e99299a806537a87501b6573a134dc58
rezajalilyan-diana <reza.jalilyan@gmail.com>
2026-06-12 16:37:25 +0330

    Refactor SagerDatabase to use Migration object

--- app/src/main/java/io/nekohasekai/sagernet/database/SagerDatabase.kt
+++ app/src/main/java/io/nekohasekai/sagernet/database/SagerDatabase.kt
@@ -5,7 +5,6 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import io.nekohasekai.sagernet.SagerNet
-import java.util.concurrent.Executors
 
 @Database(
     entities = [
@@ -28,17 +27,14 @@ abstract class SagerDatabase : RoomDatabase() {
     abstract fun ruleDao(): RuleDao
 
     companion object {
-        val queryExecutor = Executors.newCachedThreadPool()
-
         val instance by lazy {
             Room.databaseBuilder(
                 SagerNet.application,
                 SagerDatabase::class.java,
                 "sager"
             )
-                .setQueryExecutor(queryExecutor)
                 .allowMainThreadQueries()
-                .addMigrations(*MIGRATIONS)
+                .addMigrations(*Migration.MIGRATIONS)
                 .build()
         }
 
@@ -47,8 +43,9 @@ abstract class SagerDatabase : RoomDatabase() {
         val profileDao get() = instance.profileDao()
         val routingDao get() = instance.routingDao()
         val ruleDao get() = instance.ruleDao()
+    }
 
-        // تعریف آرایه به صورت مستقیم در بدنه Companion Object جهت استفاده بدون نقص در KSP
+    object Migration {
         val MIGRATIONS = arrayOf<androidx.room.migration.Migration>()
     }
 }
